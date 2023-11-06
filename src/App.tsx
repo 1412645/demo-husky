30bc0d0cba5c00dcaafc313c82c5199d1a38a7c5
Le Hoang Vu <levu020196@gmail.com>
2023-11-06 17:18:22 +0700

    fsfs

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as LogoApp } from './logo.svg'
 
 function App() {
   let a = 100
-  console.log('fsfsf: ', a)
+  console.log('s: ', a)
   return (
     <div className="App w-[200px]">
       <header className="App-header">
